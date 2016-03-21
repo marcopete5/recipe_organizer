d989e9ef097d441423492165993cb79cdfa03d44
Marcus Peterson <mdabomb5@gmail.com>
2016-03-21 14:52:10 -0600

    Add print statement for demonstration

--- recipe_organizer/settings.py
+++ recipe_organizer/settings.py
@@ -17,6 +17,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
+print BASE_DIR
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
